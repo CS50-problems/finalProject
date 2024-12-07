445d706471228d627da39189a4f77113c82d352c
CS50-problems <paperaltu1@gmail.com>
2024-12-06 19:55:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,18 @@ app = Flask(__name__)
 
 @app.route('/',methods=["GET", "POST"])
 def index():
+    return render_template("index.html")
+
+@app.route('/playlist',methods=["GET", "POST"])
+def playlist():
     if request.method == "POST":
         return render_template("response.html", response = spotifyAPIapp.main(keys.spotifyID, keys.spotifyKey, keys.spotifyPlaylist))
     else:
-        return render_template("index.html")
+        return render_template("playlist.html")
+    
+@app.route('/song',methods=["GET", "POST"])
+def song():
+    if request.method == "POST":
+        return render_template("response.html", response = spotifyAPIapp.main(keys.spotifyID, keys.spotifyKey, keys.spotifyPlaylist))
+    else:
+        return render_template("song.html")
