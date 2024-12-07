768b52ade5e6f50e7483acc431bf8fa4735f7a89
CS50-problems <paperaltu1@gmail.com>
2024-12-06 19:55:33 -0500

    Update spotifyAPIapp.py

--- spotifyAPIapp.py
+++ spotifyAPIapp.py
@@ -1,8 +1,22 @@
-import os, requests
+import os, requests, spotipy
+from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 
-def main(id, key, url):
-    print(id)
-    print(key)
-    print(url)
+import keys
 
-    #response=requests.post
+def main(id=None, key=None, url=None, playList=False):
+    spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=id,client_secret=key))
+    playlist = spotify.playlist_items(url, additional_types='track')
+
+    artist_name = []
+    track_name = []
+    popularity = []
+    track_id = []
+    for i in range(0,100,20):
+        track_results = spotify.search(q='year:2018', type='track', limit=20,offset=i)
+        for i, t in enumerate(track_results['tracks']['items']):
+            artist_name.append(t['artists'][0]['name'])
+            track_name.append(t['name'])
+            track_id.append(t['id'])
+            popularity.append(t['popularity'])
+
+main(keys.spotifyID,keys.spotifyKey,keys.spotifyPlaylist)
